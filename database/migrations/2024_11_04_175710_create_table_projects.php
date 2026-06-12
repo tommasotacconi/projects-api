c80bd5a92a166ca53990885d6493491fcba892e0
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-06-12 19:30:28 +0200

    fix: Set 'arguments' field to text in projects

--- database/migrations/2024_11_04_175710_create_table_projects.php
+++ database/migrations/2024_11_04_175710_create_table_projects.php
@@ -4,29 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-  /**
-   * Run the migrations.
-   */
-  public function up(): void
-  {
-    Schema::create('projects', function (Blueprint $table) {
-      $table->id();
-      $table->string('name', 50);
-      $table->string('authors', 255);
-      $table->tinyText('arguments');
-      $table->date('start_date')->nullable(true);
-      $table->date('end_date')->nullable(true);
-      $table->timestamps();
-    });
-  }
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('authors', 255);
+            $table->text('arguments');
+            $table->date('start_date')->nullable(true);
+            $table->date('end_date')->nullable(true);
+            $table->timestamps();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   */
-  public function down(): void
-  {
-    Schema::dropIfExists('projects');
-  }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
 };
