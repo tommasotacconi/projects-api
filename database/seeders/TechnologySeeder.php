f36e698eac14821fd647ba467360a76c6e53fd91
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-06-12 19:41:37 +0200

    feat: Seed some popular technologies

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,24 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            'Javascript',
+            'Vue',
+            'React',
+            'Node.js',
+            'Express',
+            'EJS',
+            'PHP',
+            'Laravel',
+            'MySQL',
+            'Kotlin',
+            'Java',
+            'React Native',
+            'WordPress'
+        ];
+
+        foreach ($technologies as $tech) {
+            (new Technology(['name' => $tech]))->save();
+        }
     }
 }
